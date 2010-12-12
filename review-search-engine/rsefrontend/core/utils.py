83718db1e50c963d5ad859f289f1a5143269da99
alex.sorici <alex.sorici@0a87ff73-0cfe-aa10-234c-aba3fd9a78bf>
2010-12-12 23:23:50 +0000

    frontend - first search ability

---
+++ review-search-engine/rsefrontend/core/utils.py
@@ -0,0 +1,30 @@
+from lucene import \
+    QueryParser, IndexSearcher, StandardAnalyzer, SimpleFSDirectory, File, \
+    VERSION, initVM, Version
+
+from rsefrontend import jcc_jvm
+from rsefrontend.core import searcher
+
+def standard_search(query):
+    #### lucene stuff ####
+    # attach current thread to jvm
+    jcc_jvm.attachCurrentThread()
+    
+    # create analyzer
+    analyzer = StandardAnalyzer(Version.LUCENE_CURRENT)
+    
+    # create parser for user submitted query
+    parser = QueryParser(Version.LUCENE_CURRENT, "title", analyzer)
+    parser.setDefaultOperator(QueryParser.Operator.AND)
+    formatted_query = parser.parse(query)
+    scoreDocs = searcher.search(formatted_query, 50).scoreDocs
+    
+    #### create return structure ####
+    results = []
+    
+    for scoreDoc in scoreDocs:
+        doc = searcher.doc(scoreDoc.doc)
+        resentry = { "title": doc.get("title") , "content": doc.get("summary") }
+        results.append(resentry)
+        
+    return results
