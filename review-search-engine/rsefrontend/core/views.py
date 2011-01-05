fea6504059c8f33f56eb1198b0f877b20b876fd9
alex.sorici <alex.sorici@0a87ff73-0cfe-aa10-234c-aba3fd9a78bf>
2011-01-05 21:27:09 +0000

    modifications to frontend

--- review-search-engine/rsefrontend/core/views.py
+++ review-search-engine/rsefrontend/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render_to_response, get_object_or_404, HttpResponse, HttpResponseRedirect
-from utils import standard_search
+from utils import standard_search, doSearch
 
 def home(request):
     if request.method == "GET":
@@ -9,9 +9,11 @@ def search(request):
     if request.method == "POST":
         try:
             query = request.POST['query']
-            results = standard_search(query)
+            #results = standard_search(query)
+            results = doSearch(query)
             return render_to_response("results.html", {"submitted_query": query, "results" : results})
-        except:
+        except Exception, ex:
+            print ex
             return HttpResponseRedirect("/")
     else:
         return HttpResponseRedirect("/")
